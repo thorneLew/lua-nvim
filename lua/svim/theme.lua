7ee39b62233d17c2c77cbd4a5d3b44526fa237ed
Thone Liu <thorne_lew@126.com>
2022-11-01 13:48:16 +0800

    Modify translate shortcut and add quico-scope color

--- lua/svim/theme.lua
+++ lua/svim/theme.lua
@@ -1,6 +1,9 @@
-
 -- 东京之夜
 vim.cmd[[colorscheme tokyonight]]
+
+-- unblevable/quick-scope颜色设置
+vim.cmd[[highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline]]
+vim.cmd[[highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline]]
 
 -- require("catppuccin").setup {
 -- 	flavour = "macchiato" -- mocha, macchiato, frappe, latte
